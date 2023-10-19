4690ab17ccf4f4b699f2b2904f3ddd6c875108dc
maxim.b <maxim.b@intensa.ru>
2023-10-19 11:19:07 +0300

    Удалил лишние импорты

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Audit extends BaseModel
