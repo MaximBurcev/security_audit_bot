91abd79beb891896a15c8eeceffde8b2135c8d4c
maxim.b <maxim.b@intensa.ru>
2023-10-31 18:46:19 +0300

    Используем уже написанные сидеры

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Audit;
-use App\Models\AuditReport;
-use App\Models\Project;
-use App\Models\Report;
-use App\Models\User;
-use App\Models\Utility;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
-        Utility::factory(5)->create();
-        Report::factory(5)->create();
-        Project::factory(5)->create();
-        Audit::factory(5)->create();
-        AuditReport::factory(5)->create();
+        $this->call([
+            UserSeeder::class,
+            UtilitySeeder::class,
+            ReportSeeder::class,
+            ProjectSeeder::class,
+            AuditSeeder::class,
+            AuditReportSeeder::class
+        ]);
     }
 }
