814b0285604590ea1734ab5e3c4e56dfa3807aec
maxim.b <maxim.b@intensa.ru>
2023-12-08 18:54:39 +0300

    add git clone

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,6 @@
 @servers(['web' => '82.146.58.115'])
 
 @task('deploy')
-    cd /var/www/bot
-    git pull origin master
+    cd /var/www
+    git clone git@github.com:MaximBurcev/security_audit_bot.git
 @endtask
