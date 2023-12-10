9728a53dbbeffca83be33fcb2a1ddbec9fb3208f
maxim.b <maxim.b@intensa.ru>
2023-12-10 16:28:19 +0300

    ДЗ: Deployment

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,46 @@
-@servers(['web' => '82.146.58.115'])
+@servers(['web' => 'security_audit_bot@82.146.58.115'])
 
-@task('deploy')
-    cd /var/www
-    git clone git@github.com:MaximBurcev/security_audit_bot.git
+@setup
+    $projectRoot = '/var/www/security_audit_bot/www';
+@endsetup
+
+@story('deploy')
+    update-code
+    update-dependencies
+    run-migrations
+    key-generate
+    run-tests
+@endstory
+
+@task('update-code')
+    cd {{ $projectRoot }}
+    git pull origin master
 @endtask
+
+@task('update-dependencies')
+    cd {{ $projectRoot }}
+    php /usr/local/bin/composer update
+@endtask
+
+@task('run-migrations')
+    cd {{ $projectRoot }}
+    php artisan migrate
+@endtask
+
+@task('key-generate')
+    cd {{ $projectRoot }}
+    php artisan key:generate
+@endtask
+
+@task('npm')
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    cat .bashrc
+    source .bashrc
+    nmv
+@endtask
+
+@task('run-tests')
+    cd {{ $projectRoot }}
+    php artisan test
+@endtask
+
