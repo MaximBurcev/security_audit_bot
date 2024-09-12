f13ac71f0fa70969646ec1f806ac82c2f8f41cf9
maxim.b <maxim.b@intensa.ru>
2024-09-12 18:13:39 +0300

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/users', UserController::class)->middleware('auth:api');
 
 Route::middleware('auth:api')->get('/test', function (Request $request) {
-    return 'Authenticated!';
+    return 'Authenticated!2';
 });
 
 
